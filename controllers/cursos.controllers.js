31561d0adac782bf0b870eb3d5b0f23c309ce5ab
juz2019076 <juz-2019076@kinal.edu.gt>
2024-02-18 23:00:03 -0600

    Terminado de controllers

--- controllers/cursos.controllers.js
+++ controllers/cursos.controllers.js
@@ -1,40 +1,25 @@
 const Curso = require('../models/curso');
 const { response, request } = require('express');
-const Maestro = require('../models/maestro');
-
-const cursosPost = async (req, res) => {
-    const {nombre, descripcion} = req.body;
-    const curso = new Curso({nombre, descripcion});
-
-    await curso.save();
-    res.status(202).json({
-        curso
-    });
-
-}
+const Profesor = require('../models/profesor');
 
 const cursosGet = async (req, res = response) => {
     const {limite, desde} = req.query;
     const query = {estado: true};
-
     const [total, cursos] = await Promise.all([
         Curso.countDocuments(query),
         Curso.find(query)
         .skip(Number(desde))
         .limit(Number(limite))
     ]);
-
     res.status(200).json({
         total,
         cursos
     });
-
 }
 
 const getCursoById = async (req, res) => {
     const {id} = req.params;
     const cursos = await Curso.find({_id: id});
-
     res.status(200).json({
         cursos
     });
@@ -43,9 +28,6 @@ const getCursoById = async (req, res) => {
 const putCursos = async (req, res = response) => {
     const { id } = req.params;
     const { _id, ...resto } = req.body;
-    
-  
-
     const curso = await Curso.findByIdAndUpdate(id, resto);
 
     res.status(200).json({
@@ -57,13 +39,24 @@ const putCursos = async (req, res = response) => {
 const cursosDelete = async (req, res = response) => {
     const {id} = req.params;
     const curso = await Curso.findByIdAndUpdate(id, {estado: false});
-    const usuarioAutenticado = req.maestro;
+    const usuarioAutenticado = req.profesor;
 
     res.status(200).json({
         msg: 'Curso A Eliminar',
         curso,
         usuarioAutenticado
     });
+}
+
+const cursosPost = async (req, res) => {
+    const {nombre, descripcion} = req.body;
+    const curso = new Curso({nombre, descripcion});
+
+    await curso.save();
+    res.status(202).json({
+        curso
+    });
+
 }
 
 module.exports = {
